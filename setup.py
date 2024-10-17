151cbe233c33e0965956485f4e7126a027b1c343
24aagam <aagamshah24603@gmail.com>
2024-10-17 10:17:29 +0530

    Myntra Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='Aagam',
+    author_email='velakoc316@jofuso.com',
+    packages=find_packages(),
+    install_requires=[]
+)
